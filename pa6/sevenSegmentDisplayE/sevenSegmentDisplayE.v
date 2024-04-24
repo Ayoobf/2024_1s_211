bd63af0d2ffbae02feb2e60ca3cda3acc987bd29
Ayoobf <rflooivl@gmail.com>
2024-04-24 16:51:34 -0400

    solved part 2 (Seven Segment E)

--- pa6/sevenSegmentDisplayE/sevenSegmentDisplayE.v
+++ pa6/sevenSegmentDisplayE/sevenSegmentDisplayE.v
@@ -15,6 +15,12 @@ wire minterm_08;
 wire minterm_00_or_minterm_02;
 wire minterm_06_or_minterm_08;
 
+wire not_3_and_not_2;
+wire not_1_and_not_0;
+wire is_1_and_not_0;
+wire not_3_and_is_2;
+wire is_3_and_not_2;
+
 assign not_numeral_bit_3 = ~ numeral_bit[3];
 assign not_numeral_bit_2 = ~ numeral_bit[2];
 assign not_numeral_bit_1 = ~ numeral_bit[1];
@@ -22,7 +28,16 @@ assign not_numeral_bit_0 = ~ numeral_bit[0];
 
 assign not_3_and_not_2 = not_numeral_bit_3 & not_numeral_bit_2;
 assign not_1_and_not_0 = not_numeral_bit_1 & not_numeral_bit_0;
+assign is_1_and_not_0 = numeral_bit[1] & not_numeral_bit_0;
+assign not_3_and_is_2 = not_numeral_bit_3 & numeral_bit[2];
+assign is_3_and_not_2 = numeral_bit[3] & not_numeral_bit_2;
 
 assign minterm_00 = not_3_and_not_2 & not_1_and_not_0;
+assign minterm_02 = not_3_and_not_2 & is_1_and_not_0;
+assign minterm_06 = not_3_and_is_2 & is_1_and_not_0;
+assign minterm_08 = is_3_and_not_2 & not_1_and_not_0;
+
+assign minterm_00_or_minterm_02 = minterm_00 | minterm_02;
+assign minterm_06_or_minterm_08 = minterm_06 | minterm_08;
 
 assign e = minterm_00_or_minterm_02 | minterm_06_or_minterm_08;
