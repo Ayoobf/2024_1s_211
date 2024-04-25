6ef5b0023e53079e936f7420aec216595062f9cb
Ayoobf <rflooivl@gmail.com>
2024-04-24 20:23:11 -0400

    Solved part 3

--- pa6/sevenSegmentDisplayC/sevenSegmentDisplayC.v
+++ pa6/sevenSegmentDisplayC/sevenSegmentDisplayC.v
@@ -9,27 +9,20 @@ wire not_numeral_bit_0;
 
 wire maxterm_02;
 
-wire 2_or_not_1;
-wire paren_2_or_not_1_or_0;
-wire not_3_and_paren;
-
-wire not_2_or_not_1;
-wire not_2_or_not_1_or_not_0;
-
+wire not_3_and_not_2;
+wire 1_and_0;
+wire parenthesis;
 
 assign not_numeral_bit_3 = ~ numeral_bit[3];
 assign not_numeral_bit_2 = ~ numeral_bit[2];
 assign not_numeral_bit_1 = ~ numeral_bit[1];
 assign not_numeral_bit_0 = ~ numeral_bit[0];
 
-assign 2_or_not_1 = numeral_bit[2] | not_numeral_bit_1;
-assign paren_2_or_not_1_or_0 = 2_or_not_1 | numeral_bit[0];
-assign not_3_and_paren = not_numeral_bit_3 & paren_2_or_not_1_or_0;
+assign not_3_and_not_2 = not_numeral_bit_3 & not_numeral_bit_2;
+assign 1_and_0 = numeral_bit[1] & not_numeral_bit_0;
+assign parenthesis = not_3_and_not_2 & 1_and_0;
 
-assign not_2_or_not_1 = not_numeral_bit_2 | not_numeral_bit_1;
-assign not_2_or_not_1_or_not_0 = not_2_or_not_1 | not_numeral_bit_0;
-
-assign maxterm_02 = not_3_and_paren | not_2_or_not_1_or_not_0;
+assign maxterm_02 = ~parenthesis;
 
 
 assign c = maxterm_02;
